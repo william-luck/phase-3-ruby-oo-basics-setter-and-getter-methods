465c966ac0568fb190c5d9f66fc36e078151a592
William Luck <luck.william.h@gmail.com>
2022-08-05 18:12:51 -0500

    commit tree 97a808622c221a4e1a359423b0f137b6dcde790e

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # setter
+    def name=(name)
+        @name = name
+    end
+
+    # getter
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed 
+        @breed
+    end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
 
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job_title)
+        @job = job_title
+    end
+
+    def job
+        @job
+    end
+    
+
 end
